82f483b32baf6aea84cfbf925666df9d8c0ebf92
baidoolot123 <79135938+baidoolot123@users.noreply.github.com>
2021-04-10 20:06:02 +0600

    updated

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from django.core.paginator import Paginator
 
 def product_list(request, category_slug=None): 
     category = None
@@ -10,10 +11,17 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
+    paginator = Paginator(products, 2)
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
+
+
+
     return render(request,'shop/product_list.html',
         {'category': category,
         'categories': categories,
         'products': products})
+        
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
