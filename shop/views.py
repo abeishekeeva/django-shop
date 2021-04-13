f4533a3e66c6aa2485c07822531f147efa00d91e
Ryskeldi <riskeldi.cholponkulov@gmail.com>
2021-04-13 18:14:00 +0600

    Обновления Рыскелди

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from django.core.paginator import Paginator
 
 def product_list(request, category_slug=None): 
     category = None
@@ -9,6 +10,10 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+
+    paginator = Paginator(products, 12)
+    page_namber = request.GET.get('page')
+    products = paginator.get_page(page_namber)
 
     return render(request,'shop/product_list.html',
         {'category': category,
