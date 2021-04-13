bf4725e50432648911190046998880f37c6b676d
sultan3005 <sultandzusupov888@gmail.com>
2021-04-13 17:53:14 +0600

    product paginator

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+
+from django.conf import settings
+from  django.conf.urls.static import static
 app_name = 'shop'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list,name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail,name='product_detail'),
-]
+    path('contact/', views.contact_list, name = 'contact_list'),
+] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from django.core.paginator import Paginator
 
-def product_list(request, category_slug=None): 
+def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.all()
@@ -9,6 +10,11 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+
+    paginator = Paginator(products,2)
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
+
 
     return render(request,'shop/product_list.html',
         {'category': category,
@@ -18,3 +24,6 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, 'shop/detail.html', {'product': product})
+
+def contact_list(request):
+    return render(request, 'shop/contact.html')
