20665bf8fd4151c00d4219dfda1d11537006d616
Asel <aselya.0012@yahoo.com>
2021-04-13 18:27:48 +0600

    shop

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list,name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail,name='product_detail'),
+# path()
 ]
