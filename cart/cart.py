ddef2e56a1f0ea2a0e2065ee612f0caa5947005f
Asel <aselya.0012@yahoo.com>
2021-04-20 18:07:26 +0600

    shop

--- cart/cart.py
+++ cart/cart.py
@@ -1,14 +1,16 @@
+from decimal import Decimal
+
 from shop.models import Product
+from django.conf import settings
 
 class Cart:
 
-    def __init__(self, request, product):
+    def __init__(self, request,):
         self.session= request.session
-        {'CART':{}}
-        cart= self.session.get(CART_SESSION_ID)
+        cart= self.session.get(settings.CART_SESSION_ID)
         if cart is None:
-            cart= self.session[CART_SESSION_ID]=}{}{'CART':{}}
-            self.cart=cart
+            cart= self.session[settings.CART_SESSION_ID]={}
+        self.cart=cart
 
     def add(self, product, quantity=1, override_quantity=False):
         product_id=str(product.id)
@@ -32,8 +34,8 @@ class Cart:
     def len(self):
         total_quantity= 0
         products= self.cart
-        for product in products:
-            total_quantity+= products['quantity']
+        for product in products.values():
+            total_quantity+= product['quantity']
         return total_quantity
 
     def clear(self):
@@ -43,3 +45,26 @@ class Cart:
 
     def save(self):
         self.session.modified= True
+
+    def total_price(self):
+        total_price=0
+        products = self.cart
+        for product in products.value():
+            total_price+= (product['quantity']*product['price'])
+        return total_price
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        # get the product objects and add them to the cart
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+        item['total_price'] = item['price'] * item['quantity']
+        yield item
+
+
+
+
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .cart import Cart
+from shop.models import Product
+from .forms import ProductAddForm
 
-# Create your views here.
+
+def cart_detail(request):
+    cart= Cart(request)
+    return render(request, 'cart/cart.html',{"cart":cart})
+
+def cart_add_form_main(request, product_id):
+    cart= Cart(request)
+    product= Product.objects.get(pk=product_id)
+    return redirect('cart:cart_detail')
+
+def cart_add_product(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, product_id)
+    addProductForm = ProductAddForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+    cart.add(product, quantity=cd['quantity'], override_quantity=cd['override'])
+
+    return redirect('cart:cart_detail')
+
+
