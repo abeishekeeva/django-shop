da5ba77a5dff79b9a24f2b06a7fda75b25dbb69e
baidoolot123 <79135938+baidoolot123@users.noreply.github.com>
2021-05-09 10:23:49 +0600

    yet another commit

--- myshop/celery.py
+++ myshop/celery.py
@@ -1,16 +1,10 @@
-from __future__ import absolute_import
-import os
-from celery import Celery
-from django.conf import settings 
+# import os
+# # from celery import Celery
 
+# # set the default Django settings module for the 'celery' program.
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+# # app = Celery('myshop')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
-
-app = Celery('myshop')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+# # Load task modules from all registered Django app configs.
+# app.autodiscover_tasks()
