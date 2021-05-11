d213f7377956f4706cc1a6aa184d254880ac2f79
Your Name <you@example.com>
2021-05-11 15:02:28 +0300

    myshop

--- cart/context_processor.py
+++ cart/context_processor.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-from .cart import Cart
-
-def cart(request):
-    cart= Cart(request)
-=======
 from .cart import Cart 
 
 def cart(request):
     cart = Cart(request)
->>>>>>> 1cd8cb2885fa20310957184f0f67ba12858b7678
     return {'cart': cart}
